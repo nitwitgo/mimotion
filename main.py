0712b47387a3a67ef96772f452aef48256426250
fuckgm <24849470+fuckgm@users.noreply.github.com>
2021-03-11 15:15:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -67,8 +67,8 @@ def main(user, passwd, step, sckey):
         return
     
     if step == '':
-        print ("已设置为随机步数（10000-19999）")
-        step = str(random.randint(10000,19999))
+        print ("已设置为随机步数（98000-99989）")
+        step = str(random.randint(98000,99989))
     login_token = 0
     login_token,userid = login(user,password)
     if login_token == 0:
